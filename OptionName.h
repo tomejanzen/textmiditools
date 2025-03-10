41ef28f3219ad8793450a53381b6b86d7462096e
Thomas E. Janzen <88851664+tomejanzen@users.noreply.github.com>
2025-03-10 19:05:38 -0400

    Add files via upload
    neglected OptionName.h, i had not added it to the kit

---
+++ OptionName.h
@@ -0,0 +1,48 @@
+//
+// TextMIDITools Version 1.0.92
+//
+// textmidi 1.0.6
+// Copyright © 2025 Thomas E. Janzen
+// License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>
+// This is free software: you are free to change and redistribute it.
+// There is NO WARRANTY, to the extent permitted by law.
+//
+#if !defined(OPTIONNAME_H)
+#    define  OPTIONNAME_H
+
+#include <string>
+#include <string_view>
+
+namespace textmidi {
+
+class OptionName
+{
+  public:
+    constexpr OptionName(const std::string& option, const std::string& text,
+        char letter = 0)
+      : option_{option},
+        text_{text},
+        registered_name_{option + (letter ? (std::string{","} + letter) : "")}
+    {
+    }
+    constexpr std::string registered_name() const
+    {
+        return registered_name_;
+    }
+    constexpr std::string option() const
+    {
+        return option_;
+    }
+    constexpr std::string text() const
+    {
+        return text_;
+    }
+ private:
+    const std::string option_;
+    const std::string text_;
+    const std::string registered_name_;
+};
+
+} // namespace textmidi
+
+#endif

59895a37d78d98c34a6b1b7a3050ca28d42f6ef2
Thomas E. Janzen <88851664+tomejanzen@users.noreply.github.com>
2025-03-26 00:00:10 -0400

    Delete test/textmidicgmtest.bash

--- test/textmidicgmtest.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-textmidicgm --random random
-xmllint random.xml
-../textmidiformedit.py random.xml
-xmllint random.xml
-textmidicgm --xmlform random.xml --textmidi random.txt --instruments piano strings reed
-textmidi --textmidi random.txt --midi random.mid
-musescore random.mid
-timidity -invvvt -Os random.mid
